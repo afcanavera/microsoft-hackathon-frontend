a2b2eb30024455ab9452c668db8aacb31d1e4751
hudanadeem <hudanadeem.02@gmail.com>
2025-03-13 22:24:51 -0400

    basic structure and component functionality

--- src/pages/StatusPage/StatusPage.jsx
+++ src/pages/StatusPage/StatusPage.jsx
@@ -1,13 +1,58 @@
 import "./StatusPage.scss";
-import TemplateCopyMe from '../../components/TemplateCopyMe/TemplateCopyMe'
+import { useState, useEffect } from "react";
+import { CheckCircle, Loader } from "lucide-react";
+import logo from "../../Assets/images/logo.png";
 
 function StatusPage() {
+  const [loadingTask, setLoadingTask] = useState(true);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setLoadingTask(false);
+    }, 3000); 
+  }, []);
   return (
-    <>
-      <TemplateCopyMe />
-    </>
-  )
+    <div className="status__page">
+      <div className="status__page--container">
+        <div className="status__page--header">
+          <div className="status__page--logo">
+            <img className="status__page--icon" src={logo} alt="logo" />
+            AutoPilot+
+          </div>
+          <button className="status__page--profile">ChatBot's Name</button>
+        </div>
+        <div className="status__page--content">
+          <p className="status__page--title">Loading your request...</p>
+
+          <div className={`task ${loadingTask ? "loading" : "completed"}`}>
+            {loadingTask ? (
+              <Loader className="icon spinning" />
+            ) : (
+              <CheckCircle className="icon" />
+            )}
+            <span>Identify and resolve the issue in this code</span>
+          </div>
+
+          <div className="task completed">
+            <CheckCircle className="icon" />
+            <span>Schedule a meeting with Felipe for 5pm</span>
+          </div>
+
+          <div className="task completed">
+            <CheckCircle className="icon" />
+            <span>Set up Teams notification</span>
+          </div>
+
+          <div className="task completed">
+            <CheckCircle className="icon" />
+            <span>Create a new Excel spreadsheet</span>
+          </div>
+
+          <p className="view-requests">View All Requests</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default StatusPage
+export default StatusPage;
