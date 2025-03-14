20d7978a1b11d473a0a2abb4241adac54b0285a6
Yawen Diao <yawendiao@gmail.com>
2025-03-13 23:39:00 -0400

    form design

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,17 +1,102 @@
-export function Form(){
-  return(
-    <>
-    <h1>Tell me about yourself and what would you like to do?</h1>
-    <form>
-    <textarea name="name" id="name" placeholder = "What is your name?"></textarea>
-    <textarea name="background" id="background" placeholder = "What is your background?"></textarea>
-    <textarea name="name" id="name" placeholder = "What type of work do you want to do?"></textarea>
-    <textarea name="name" id="name" placeholder = "What format would you like?"></textarea>
-    <textarea name="name" id="name" placeholder = "When is your deadline?"></textarea>
-    <textarea name="name" id="name" placeholder = "Any additional notes?"></textarea>
-    <button type ="submit">I'm ready!</button>
-    </form>
-    </>
-  )
-};
-
+import avatar from "../../Assets/images/user4.png";
+import pencile from "../../Assets/images/edit.png";
+import "./Form.scss";
+export default function Form() {
+  return (
+    <div className="Form__background">
+      <main className="Form__main">
+        <div className="Form__user">
+          <img src={avatar} alt="" className = "Form__user-avatar"/>
+          <div className="Form__user-info">
+          <h2 className="Form__user-name">Chatbot's name</h2>
+          <h2 className="Form__user-email">Chatbot@gmail.com</h2>
+          </div>
+        </div>
+        <div className="Form__form">
+          <h1 className="Form__title">
+            Tell me about yourself and what would you like to do?
+          </h1>
+          <form className="Form__field">
+            <div className="Form__container-outer">
+              <div className="Form__container-inner">
+                <label className="Form__label" htmlFor="">
+                  What is your name?
+                </label>
+                <img src={pencile} alt="" />
+              </div>
+              <textarea
+                className="Form__textarea"
+                name="name"
+                id="name"
+              ></textarea>
+            </div>
+            <div className="Form__container-outer">
+              <div className="Form__container-inner">
+                <label className="Form__label" htmlFor="">
+                  What is your background?
+                </label>
+                <img src={pencile} alt="" />
+              </div>
+              <textarea
+                className="Form__textarea"
+                name="background"
+                id="background"
+              ></textarea>
+            </div>
+            <div className="Form__container-outer ">
+              <div className="Form__container-inner">
+                <label className="Form__label" htmlFor="">
+                  What type of work do you want to do?
+                </label>
+                <img src={pencile} alt="" />
+              </div>
+              <textarea
+                className="Form__textarea"
+                name="name"
+                id="name"
+              ></textarea>
+            </div>
+            <div className="Form__container-outer">
+              <div className="Form__container-inner">
+                <label htmlFor="" className="Form__label">
+                  What format would you like?
+                </label>
+                <img src={pencile} alt="" />
+              </div>
+              <textarea name="name" id="name" className="Form_tex"></textarea>
+            </div>
+            <div className="Form__container-outer">
+              <div className="Form__container-inner">
+                <label htmlFor="" className="Form__label">
+                  When is your deadline?
+                </label>
+                <img src={pencile} alt="" />
+              </div>
+              <textarea
+                name="name"
+                id="name"
+                className="Form__textarea"
+              ></textarea>
+            </div>
+            <div className="Form__container-outer">
+              <div className="Form__container-inner">
+                <label htmlFor="" className="Form__label">
+                  Any additional notes?
+                </label>
+                <img src={pencile} alt="" />
+              </div>
+              <textarea
+                name="name"
+                id="name"
+                className="Form__textarea"
+              ></textarea>
+            </div>
+            <button type="submit" className="Form__button">
+              I'm ready!
+            </button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+}
--- src/pages/FormPage/FormPage.jsx
+++ src/pages/FormPage/FormPage.jsx
@@ -6,7 +6,6 @@ function FormPage() {
 
   return (
     <>
-      
       <Form />
     </>
   )
